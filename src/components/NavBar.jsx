b67f80f52dfe4554ee659ac6a6f80994b969f34b
@Nagendra_Yaparala <yaparalanagendra@gmail.com>
2024-08-11 15:16:51 +0530

    after Project-Setup

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+const navData = ['Home','Courses','']
+
+const NavBar = () => {
+  return (
+    <nav className=' bg-white text-black shadow-md shadow-slate-200 p-6'>
+        <h1 className='font-semibold'>AspireGOV</h1>
+        <div className=''>
+
+        </div>
+    </nav>
+  )
+}
+
+export default NavBar
