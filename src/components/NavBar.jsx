086aa2a71fd4e8910675df7d273b49e51a913de0
@Nagendra_Yaparala <yaparalanagendra@gmail.com>
2024-08-15 10:02:22 +0530

    after-creation-of-navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
-const navData = ['Home','Courses','']
+const navData = ['Courses','Platforms','Resources','About']
 
 const NavBar = () => {
   return (
-    <nav className=' bg-white text-black shadow-md shadow-slate-200 p-6'>
+    <div className=' bg-[#6134ea] text-white shadow-md shadow-slate-200 p-6 flex items-center justify-between px-40'>
         <h1 className='font-semibold'>AspireGOV</h1>
-        <div className=''>
-
+        <div className='flex gap-10 hover:'>
+        {navData.map((item,index)=>{
+          return(<span className='' key={index}>{item}</span>)
+        })}
         </div>
-    </nav>
+    </div>
   )
 }
 
